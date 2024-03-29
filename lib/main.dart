271cc23f365ab9968f097705c82bd2e1252f2f64
sham163 <sh.prin1698@gmail.com>
2024-03-29 04:28:28 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_adaptive_dashboard/views/dashboard_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ResponsiveDashboard());
 }
 
+class ResponsiveDashboard extends StatelessWidget {
+  const ResponsiveDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DashboardView(),
+    );
+  }
+}
